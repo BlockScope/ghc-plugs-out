f5183b7f8ed4de6f63ed0b3d49964cb7da5f6de6
Phil de Joux <philderbeast@gmail.com>
2022-03-19 11:32:03 -0400

    Go with two test suites, in-turn and in-line.

--- test-suites/counter-in-turn/Main.hs
+++ test-suites/counter-in-turn/Main.hs
@@ -3,11 +3,6 @@
 {-# OPTIONS_GHC     -fplugin CallCount.Pure.Plugin #-}
 {-# OPTIONS_GHC -fplugin-opt CallCount.Pure.Plugin:B #-}
 
-{-# OPTIONS_GHC -fclear-plugins #-}
-
-{-# OPTIONS_GHC -fplugin CallCount.Pure.Plugin -fplugin-opt CallCount.Pure.Plugin:A #-}
-{-# OPTIONS_GHC -fplugin CallCount.Pure.Plugin -fplugin-opt CallCount.Pure.Plugin:B #-}
-
 module Main where
 
 foo :: IO a
